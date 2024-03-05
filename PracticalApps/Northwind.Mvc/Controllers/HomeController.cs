b3baa67f42630ed200a376734ecf4dd8bcdf16e2
Alexander Butko <thebadgroover@gmail.com>
2024-03-05 12:08:25 +0300

    add mvc

---
+++ PracticalApps/Northwind.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Mvc.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Northwind.Mvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [ResponseCache(Duration = 10, Location = ResponseCacheLocation.Any, NoStore=false)]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [Route("private")]
+    [Authorize(Roles = "Administrators")]
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
